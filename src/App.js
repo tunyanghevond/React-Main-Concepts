da072a4c1c5f6a1991c59329b5f4ebc46aac2182
Ghevond Tunyan <ghtunyan2018@gmail.com>
2021-07-29 16:49:57 +0200

    useContext  and custom hhoks concepts are done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import Setup from "./tutorial/6-useReducer/setup";
-
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
